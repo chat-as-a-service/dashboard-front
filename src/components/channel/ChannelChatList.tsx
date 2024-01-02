400aa4359d9852f06e1362e0c444ff4d6dd8f8ed
Daniel Kim <yuki9447@colorado.edu>
2024-01-01 23:04:14 -0700

    Fix errors

--- src/components/channel/ChannelChatList.tsx
+++ src/components/channel/ChannelChatList.tsx
@@ -5,6 +5,8 @@ import { UserListRes } from '../../types/user';
 import { ReactionOpType } from '../../types/reaction';
 import { Empty, notification, Spin } from 'antd';
 import styled from 'styled-components';
+import dayjs from 'dayjs';
+import MessageLine from '../message/MessageLine';
 
 const Box = styled.div`
   display: flex;
@@ -46,7 +48,9 @@ const ChannelChatList = ({
   return (
     <Box>
       {notificationContextHolder}
-      {messages.length === 0 && !loading && <Empty style={{ margin: '50px 0' }} description="No 'essages" />'
+      {messages.length === 0 && !loading && (
+        <Empty style={{ margin: '50px 0' }} description="No Messages" />
+      )}
       {messages.map((message, index) => {
         let displayFull = false;
         let firstMsgOfTheDay = false;
@@ -88,7 +92,7 @@ const ChannelChatList = ({
         );
       })}
     </Box>
-  )
+  );
 };
 
 export default observer(ChannelChatList);
